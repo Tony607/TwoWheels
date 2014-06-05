e8ee7643238a15238587965cad9a322339eb6c5d
chengwei.zhang <chengwei.zhang@intelligrated.com>
2014-06-05 15:40:48 -0400

    add new test

--- T3Test/T3Test.ino
+++ T3Test/T3Test.ino
@@ -1,10 +1,8 @@
+//new test
 #define CAPTURE 13
 #define TRIG 3
-//just a comment
 #define ledPin 2
-//new pull
 volatile unsigned int pulseWidth = 2013;
-//new copy
 void setup()
 {
   pinMode(CAPTURE, INPUT);
