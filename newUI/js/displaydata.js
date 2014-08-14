21eba3b31154ee93d3a7a06572ad34c869b9f9cf
Tony607 <zcw607@gmail.com>
2014-08-14 19:35:41 -0400

    remove flot to save cpu

--- newUI/js/displaydata.js
+++ newUI/js/displaydata.js
@@ -3,7 +3,6 @@ var displaydata = (function () {
 	var totalPoints = 1000;
 	var resX = [];
 	var resY = [];
-	var plot;
 	var calibrateButton;
 	getInitData = function () {
 		// zip the generated y values with the x values
@@ -13,58 +12,15 @@ var displaydata = (function () {
 		}
 		return [resX, resY];
 	}
-	// Options for Flot plot
-	var options = {
-		series : {
-			shadowSize : 0
-		}, // drawing is faster without shadows
-		yaxis : {
-			min : -180,
-			max : 180
-		},
-		xaxis : {
-			show : false
-		}
-	};
 
 	init = function () {
-		plot = $.plot($("#placeholder"), getInitData(), options);
-		$("#progressbar").progressbar({
-			value : 0,
-			 max: 360
-		});
 		calibrateButton = $("#docalibrate")
-	}
-	updatedisplay = function (valX, valY) {
-		// Put sensor value to the 'sensor_value' span
-		$('#sensor_value').html(parseFloat(valX).toFixed(2));
-
-		// Push new value to Flot Plot
-		resX.push([totalPoints, valX]); // push on the end side
-		resX.shift(); // remove first value to maintain 300 points
-		resY.push([totalPoints, valY]); // push on the end side
-		resY.shift(); // remove first value to maintain 300 points
-		// reinitialize the x axis data points to 0 to 299.
-		for (i = 0; i < totalPoints; i++) {
-			resX[i][0] = i;
-			resY[i][0] = i;
-		}
-		// Redraw the plot
-		plot.setData([resX, resY]);
-		plot.draw();
-
-		// Update JQuery UI progress bar.
-		$("#progressbar").progressbar({
-			value : valX+180
-		});
-
 	}
 	getcalibrateButton = function () {
 		return calibrateButton;
 	}
 	return {
 		init : init,
-		updatedisplay : updatedisplay,
 		calibrateButton : getcalibrateButton
 	}
 })();
